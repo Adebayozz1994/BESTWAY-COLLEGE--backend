587472a0bf8e7cb8a11ad7243979d697b88ff8e5
ogunlade adebayo peter <ogunladeadebayopeter@gmail.com>
2024-02-16 12:13:31 -0100

    new file

---
+++ routes/user.route.js
@@ -0,0 +1,18 @@
+const express = require ("express")
+const router = express.Router();
+
+const {displayWelcome, login, register} = require("../controllers/user.controller")
+
+
+
+
+
+router.get("/", displayWelcome);
+router.post("/register", register);
+router.post("/login", login);
+
+
+
+
+
+module.exports = router;
